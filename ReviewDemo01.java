6c7304cdc32f386442d0d5bf14223e9c6da6c767
kws06198 <kws06198@naver.com>
2021-09-16 01:33:53 +0900

    version 04

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -7,9 +7,13 @@ public class ReviewDemo01 {
 		int number = sc.nextInt();
 		boolean isPrime = true;
 		
-		for(int k=2; k<number; k++) {
-			if(number % k == 0)
+		int k=2;
+		while(k<number) {
+			if(number % k == 0) {
 				isPrime = false;
+				break;
+			}
+			k++;
 		}
 		if(isPrime)
 			System.out.println(number + "은 소수다.");
